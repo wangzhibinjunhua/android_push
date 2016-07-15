ee233e1daddfc73bab841dbd1fe3ca39d726858b
wzb <wangzhibin_x@qq.com>
2016-07-15 17:22:36 +0800

    根据id查询是否有最新消息

--- src/com/wzb/androidpush/PushService.java
+++ src/com/wzb/androidpush/PushService.java
@@ -1,7 +1,11 @@
 package com.wzb.androidpush;
 
 
+import java.util.HashMap;
+import java.util.Map;
+
 import cn.trinea.android.common.util.HttpUtils;
+import cn.trinea.android.common.util.JSONUtils;
 import android.R.integer;
 import android.app.PendingIntent;
 import android.app.Service;
@@ -13,6 +17,7 @@ import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
 import android.os.SystemClock;
+import android.text.TextUtils;
 import android.util.Log;
 import android.os.SystemProperties;
 
@@ -22,10 +27,13 @@ import android.os.SystemProperties;
 public class PushService extends Service {
 	
 	private static final String ANDROID_PUSH_HTTP_URL_NOVATECH="persist.sys.push_url_novatech";
-	private static final String ANDROID_PUSH_HTTP_URL_NOVATECH_DEFAULT="http://lib.huayinghealth.com/";
+	private static final String ANDROID_PUSH_HTTP_URL_NOVATECH_DEFAULT="http://api.huayinghealth.com/push.php";
 	
 	private static final String ANDROID_PUSH_INTERVAL_NOVATECH="persist.sys.push_int_novatech";
+	private static final String ANDROID_PUSH_ID_NOVATECH="persist.sys.push_id_novatech";
+	private static final int ANDROID_PUSH_ID_NOVATECH_DEFAULT=0;
 	private static final int ANDROID_PUSH_INTERVAL_NOVATECH_DEFAULT=3; //s default 3minutes
+	private static final int ANDROID_PUSH_INTERVAL_NOVATECH_MAX=900*1000;//max interval 15minutes
 	private Context mContext;
 	private int requestCode = (int) SystemClock.uptimeMillis();
 	@Override
@@ -87,13 +95,19 @@ public class PushService extends Service {
 			public void run() {
 				Message msg = new Message();
 				Bundle data = new Bundle();
-				rs=HttpUtils.httpGetString(http_url);
+				int notify_id=SystemProperties.getInt(ANDROID_PUSH_ID_NOVATECH, ANDROID_PUSH_ID_NOVATECH_DEFAULT);
+				Map<String,String> map=new HashMap<String, String>();
+				map.put("id",""+notify_id);
+				
+				String http_url_paras=HttpUtils.getUrlWithParas(http_url, map);
+				rs=HttpUtils.httpGetString(http_url_paras);
 				//Log.d("wzb","url="+http_url+" rs="+rs);
 				data.putString("value", rs);
 				msg.setData(data);
 				msg.what = 0xf1;
 				mHandler.sendMessage(msg);
 				int interval=SystemProperties.getInt(ANDROID_PUSH_INTERVAL_NOVATECH, ANDROID_PUSH_INTERVAL_NOVATECH_DEFAULT)*1000;
+				interval=interval>ANDROID_PUSH_INTERVAL_NOVATECH_MAX? ANDROID_PUSH_INTERVAL_NOVATECH_MAX:interval;
 				mHandler.sendEmptyMessageDelayed(0xff, interval);
 
 			}
@@ -105,20 +119,36 @@ public class PushService extends Service {
 	}
 	
 	void handle_event(String msg){
-		show_notify();
+		int interval=JSONUtils.getInt(msg, "interval", 3);
+		//Log.d("wzb","interval="+interval);
+		if(interval>3){
+			SystemProperties.set(ANDROID_PUSH_INTERVAL_NOVATECH, ""+interval);
+		}
+		int id=JSONUtils.getInt(msg, "id",0);
+		if(id>0){
+			SystemProperties.set(ANDROID_PUSH_ID_NOVATECH, ""+id);
+		}
+		String ticker=JSONUtils.getString(msg, "ticker","");
+		//Log.d("wzb","ticker="+ticker);
+		String title=JSONUtils.getString(msg, "title","");
+		//Log.d("wzb","title="+title);
+		String content=JSONUtils.getString(msg, "content","");
+		//Log.d("wzb","content="+content);
+		String url=JSONUtils.getString(msg, "url","");
+		//Log.d("wzb","url="+url);
+		if(!TextUtils.isEmpty(content) && !TextUtils.isEmpty(title)){
+			show_notify(ticker,title,content,url);
+		}
+		
 	}
 	
-	private void show_notify() {
-		final Uri uri = Uri.parse("http://www.baidu.com");
+	private void show_notify(String ticker,String title,String content,String url) {
+		final Uri uri = Uri.parse(url);
 		Intent intent = new Intent(Intent.ACTION_VIEW, uri);
 		intent.setFlags(Intent.FLAG_ACTIVITY_SINGLE_TOP);
 		PendingIntent pIntent = PendingIntent.getActivity(mContext,
 				requestCode, intent, PendingIntent.FLAG_UPDATE_CURRENT);
 		int smallIcon = R.drawable.ic_launcher;
-		String ticker = "您有一条新通知";
-		String title = "朱立伦请辞国民党主席 副主席黄敏惠暂代党主席";
-		String content = "play.google.com/store/apps/developer?id=Microsoft+Corporation据台湾“中央社”报道，国民党主席朱立伦今天(18日)向中常会报告，为败选请辞党主席一职，他感谢各位中常委的指教包容，也宣布未来党务工作由副主席黄敏惠暂代，完成未来所有补选工作。";
-		// 实例化工具类，并且调用接口
 		NotifyUtil notify2 = new NotifyUtil(mContext, 2);
 		notify2.notify_normail_moreline(pIntent, smallIcon, ticker, title,
 				content, true, true, false);
